09cf75197bae7c9240e5469eb06bb12de0dc21ac
Valerai Dementyeva <lera.dementjeva@yandex.ru>
2023-08-09 12:26:11 +0300

    Initial Commit

---
+++ RGBAppTEST/RGBAppTESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBAppTESTApp.swift
+//  RGBAppTEST
+//
+//  Created by Валерия Дементьева on 09.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct RGBAppTESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
